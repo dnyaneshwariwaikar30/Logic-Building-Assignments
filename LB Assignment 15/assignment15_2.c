9246804b152018bc37e7d2f11ce64bc094714926
dnyaneshwariwaikar30 <dnyaneshwariwaikar30@gmail.com>
2025-05-22 00:13:20 +0530

    Add files via upload

---
+++ LB Assignment 15/assignment15_2.c
@@ -0,0 +1,63 @@
+//Q-2 Accept the number of rows and number of colums from user and display below pattern.
+//Input  : iRow = 4     iCol = 4
+//Output : 
+//          *   *   *   *
+//          *   *   *   #   
+//          *   *   #   #
+//          *   #   #   #
+
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name  : Pattern
+// Description    : Display pattern
+// Input          : Integer, Integer
+// Output         : -
+// Author         : Dnyaneshwari Makarand Waikar
+// Date           : 21/5/25
+// 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = iRow; i >= 1; i--)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i < j)
+            {
+                printf("#\t");
+            }
+            else 
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// This application is accept the number from user and display pattern.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the number of columns : \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
